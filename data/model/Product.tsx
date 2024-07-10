654f08de3463234e938bf2c7da7d5714ce5c7596
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-10 20:44:38 +0700

    create product model

---
+++ data/model/Product.tsx
@@ -0,0 +1,17 @@
+
+export interface Product {
+    id: number;
+    title: string;
+    price: number;
+    description: string;
+    images: string[];
+    creationAt: string;
+    updatedAt: string;
+    category: {
+      id: number;
+      name: string;
+      image: string;
+      creationAt: string;
+      updatedAt: string;
+    };
+  }
