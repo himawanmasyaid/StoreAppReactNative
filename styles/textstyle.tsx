c9448b745adb94d22efa177eecab0924106f149e
himawan.masyaid <himawan.masyaid@gmail.com>
2024-07-11 19:34:27 +0700

    create : text style

---
+++ styles/textstyle.tsx
@@ -0,0 +1,201 @@
+
+
+const Regular = {
+  fontFamily: 'poppins_regular',
+  color: "#333333",
+};
+
+const SemiBold = {
+  fontFamily: 'poppins_semibold',
+  color: "#333333",
+};
+
+const Bold = {
+  fontFamily: 'poppins_bold',
+  color: "#333333",
+};
+
+
+const textstyle = {
+
+    Regular,
+    SemiBold,
+    Bold,
+  
+    Header1: {
+      ...Regular,
+      fontSize: 96,
+    },
+  
+    Header1Medium: {
+      ...SemiBold,
+      fontSize: 96,
+    },
+  
+    Header1Bold: {
+      ...Bold,
+      fontSize: 96,
+    },
+  
+    Header2: {
+      ...Regular,
+      fontSize: 60,
+    },
+  
+    Header2Medium: {
+      ...SemiBold,
+      fontSize: 60,
+    },
+  
+    Header2Bold: {
+      ...Bold,
+      fontSize: 60,
+    },
+  
+    Header3: {
+      ...Regular,
+      fontSize: 48,
+    },
+  
+    Header3Medium: {
+      ...SemiBold,
+      fontSize: 48,
+    },
+  
+    Header3Bold: {
+      ...Bold,
+      fontSize: 48,
+    },
+  
+    Header4: {
+      ...Regular,
+      fontSize: 34,
+    },
+  
+    Header4Medium: {
+      ...SemiBold,
+      fontSize: 34,
+    },
+  
+    Header4Bold: {
+      ...Bold,
+      fontSize: 34,
+    },
+  
+    Header5: {
+      ...Regular,
+      fontSize: 24,
+    },
+  
+    Header5Medium: {
+      ...SemiBold,
+      fontSize: 24,
+    },
+  
+    Header5Bold: {
+      ...Bold,
+      fontSize: 24,
+    },
+  
+    Header6: {
+      ...Regular,
+      fontSize: 20,
+    },
+  
+    Header6Medium: {
+      ...SemiBold,
+      fontSize: 20,
+    },
+  
+    Header6Bold: {
+      ...Bold,
+      fontSize: 20,
+    },
+  
+    Header7: {
+      ...Regular,
+      fontSize: 18,
+    },
+  
+    Header7Medium: {
+      ...SemiBold,
+      fontSize: 18,
+    },
+  
+    Header7Bold: {
+      ...Bold,
+      fontSize: 18,
+    },
+  
+    TextParagraph1: {
+      ...Regular,
+      fontSize: 16,
+    },
+  
+    TextParagraph1Medium: {
+      ...SemiBold,
+      fontSize: 16,
+    },
+  
+    TextParagraph1Bold: {
+      ...Bold,
+      fontSize: 16,
+    },
+  
+    TextParagraph2: {
+      ...Regular,
+      fontSize: 14,
+    },
+  
+    TextParagraph2Medium: {
+      ...SemiBold,
+      fontSize: 14,
+    },
+  
+    TextParagraph2Bold: {
+      ...Bold,
+      fontSize: 14,
+    },
+  
+    TextParagraph3: {
+      ...Regular,
+      fontSize: 12,
+    },
+  
+    TextParagraph3Medium: {
+      ...SemiBold,
+      fontSize: 12,
+    },
+  
+    TextParagraph3Bold: {
+      ...Bold,
+      fontSize: 12,
+    },
+  
+    TextParagraph4: {
+      ...Regular,
+      fontSize: 10,
+    },
+
+    TextParagraph4Medium: {
+      ...SemiBold,
+      fontSize: 10,
+    },
+  
+    TextParagraph4Bold: {
+      ...Bold,
+      fontSize: 10,
+    },
+  
+    TextParagraph5: {
+      ...Regular,
+      fontSize: 8,
+    },
+  
+    TextParagraph6: {
+      ...Regular,
+      fontSize: 6,
+    },
+  };
+  
+  export default textstyle;
